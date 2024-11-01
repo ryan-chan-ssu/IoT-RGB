333657a0763f56fda74b3fc138d2f264cdfad255
Ryan Chan <chanrya@sonoma.edu>
2024-10-31 18:02:47 -0700

    Update updateRGB.php

--- multiLED/updateRGB.php
+++ multiLED/updateRGB.php
@@ -4,22 +4,22 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Fetch status and RGB values for LED 1
-$status1 = isset($_POST['status1']) ? $_POST['status1'] : 'off';
-$red1 = isset($_POST['redValue1']) ? intval($_POST['redValue1']) : 0;
-$green1 = isset($_POST['greenValue1']) ? intval($_POST['greenValue1']) : 0;
-$blue1 = isset($_POST['blueValue1']) ? intval($_POST['blueValue1']) : 0;
-
-// Fetch status and RGB values for LED 2
-$status2 = isset($_POST['status2']) ? $_POST['status2'] : 'off';
-$red2 = isset($_POST['redValue2']) ? intval($_POST['redValue2']) : 0;
-$green2 = isset($_POST['greenValue2']) ? intval($_POST['greenValue2']) : 0;
-$blue2 = isset($_POST['blueValue2']) ? intval($_POST['blueValue2']) : 0;
-
 // Read current JSON data from results.txt
 $filename = 'results.txt';
 $jsonData = file_get_contents($filename);
 $data = json_decode($jsonData, true);
+
+// Fetch or preserve status and RGB values for LED 1
+$status1 = isset($_POST['status1']) ? $_POST['status1'] : $data['Node1']['LED1']['status'];
+$red1 = isset($_POST['redValue1']) ? intval($_POST['redValue1']) : $data['Node1']['LED1']['red'];
+$green1 = isset($_POST['greenValue1']) ? intval($_POST['greenValue1']) : $data['Node1']['LED1']['green'];
+$blue1 = isset($_POST['blueValue1']) ? intval($_POST['blueValue1']) : $data['Node1']['LED1']['blue'];
+
+// Fetch or preserve status and RGB values for LED 2
+$status2 = isset($_POST['status2']) ? $_POST['status2'] : $data['Node1']['LED2']['status'];
+$red2 = isset($_POST['redValue2']) ? intval($_POST['redValue2']) : $data['Node1']['LED2']['red'];
+$green2 = isset($_POST['greenValue2']) ? intval($_POST['greenValue2']) : $data['Node1']['LED2']['green'];
+$blue2 = isset($_POST['blueValue2']) ? intval($_POST['blueValue2']) : $data['Node1']['LED2']['blue'];
 
 // Update data for LED 1
 $data['Node1']['LED1']['status'] = $status1;
